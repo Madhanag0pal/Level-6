77adaff7fce33da0ea292fd32d525854dd06d584
Madhanahopal N <nmadhanagopal.2000@gmail.com>
2021-06-02 11:44:37 +0530

    revert to previous commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_05_22_192629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "todos", force: :cascade do |t|
-    t.text "todo_text"
-    t.date "due_date"
-    t.boolean "completed"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
